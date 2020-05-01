d08316a9300c71206668a407dfca6e46cf6ba5e1
fpetracci <petracci.francesco@gmail.com>
2020-05-01 15:21:57 +0200

    Progress on 7R, first try at inverse kinematic. Quaternion try and error

--- Data Analysis/arm_7R_prova.m
+++ Data Analysis/arm_7R_prova.m
@@ -1,16 +1,18 @@
 %% Intro
 clear; clc; close;
+
+load('healthy.mat');
+trial = healthy_task(1).subject(1).left_side_trial(1);
+clear healthy_task;
+
+
 %% define link of our serial links manipulator
 
-P_Shoulder_R = [0.2 0 1];
-l1_r = 0.5;		% upperarm, shoulder - elbow
-l2_r= 0.5;		% forearm, elbow - wrist
-l3_r = 0.15; 	% wrist - hand
-
-P_Shoulder_L = [-0.2 0 1];
-l1_l = 0.5;		% upperarm, shoulder - elbow
-l2_l= 0.5;		% forearm, elbow - wrist
-l3_l = 0.15; 	% wrist - hand
+P_Shoulder_R = trial.P_Shoulder_R(1,:);
+P_Shoulder_L = trial.P_Shoulder_L(1,:);
+a = lengths_arm(trial);
+l_u_l = a(1); l_u_r = a(2); l_f_l = a(3); l_f_r = a(4); 
+l_hand = 0.15; 	% wrist - hand
 
 % right
 %add L1 and L8 to connect our arm to the neck and to the hand
@@ -23,11 +25,11 @@ T01_right = rt2tr(rotx(+pi/2), P_Shoulder_R');
 % serial links connection
 Link_r = [	Link('d', 0,	'a',	0,		'alpha', -pi/2),...						% spalla1-spalla2:	theta spalla pronosupinazione
 			Link('d', 0,	'a',	0,		'alpha', -pi/2, 'offset', +pi/2),...	% spalla2-spalla3:	theta spalla spazzata orizzontale
-			Link('d', 0,	'a',	l1_r,	'alpha', +pi/2),...						% spalla3-gomito:	theta spalla spazzata verticale
-			Link('d', 0,	'a',	l2_r,	'alpha', -pi/2),...						% gomito-polso1:	theta gomito
+			Link('d', 0,	'a',	l_u_r,	'alpha', +pi/2),...						% spalla3-gomito:	theta spalla spazzata verticale
+			Link('d', 0,	'a',	l_f_r,	'alpha', -pi/2),...						% gomito-polso1:	theta gomito
 			Link('d', 0,	'a',	0,		'alpha', +pi/2),...						% polso1-polso2:	theta polso pitch
 			Link('d', 0,	'a',	0,		'alpha', +pi/2, 'offset', -pi/2),...	% polso2-polso3:	theta polso yaw
-			Link('d', -l3_r,'a',	0,		'alpha', +pi, 'offset', +pi/2)];		% polso3-ee:		theta polso pronosupinazione
+			Link('d', -l_hand,'a',	0,		'alpha', +pi, 'offset', +pi/2)];		% polso3-ee:		theta polso pronosupinazione
 
 Right_Arm = SerialLink(Link_r, 'name', 'Right arm');
 Right_Arm.base = T01_right;
@@ -39,16 +41,15 @@ T01_left = rt2tr(rotx(-pi/2)*rotz(pi), P_Shoulder_L');
 % serial links connection
 Link_l = [	Link('d', 0,	'a',	0,		'alpha', +pi/2),...						% spalla1-spalla2:	theta spalla pronosupinazione
 			Link('d', 0,	'a',	0,		'alpha', +pi/2, 'offset', +pi/2),...	% spalla2-spalla3:	theta spalla spazzata orizzontale
-			Link('d', 0,	'a',	l1_l,	'alpha', -pi/2),...						% spalla3-gomito:	theta spalla spazzata verticale
-			Link('d', 0,	'a',	l2_l,	'alpha', +pi/2),...						% gomito-polso1:	theta gomito
+			Link('d', 0,	'a',	l_u_l,	'alpha', -pi/2),...						% spalla3-gomito:	theta spalla spazzata verticale
+			Link('d', 0,	'a',	l_f_l,	'alpha', +pi/2),...						% gomito-polso1:	theta gomito
 			Link('d', 0,	'a',	0,		'alpha', -pi/2),...						% polso1-polso2:	theta polso pitch
 			Link('d', 0,	'a',	0,		'alpha', -pi/2, 'offset', -pi/2),...	% polso2-polso3:	theta polso yaw
- 			Link('d', l3_l,	'a',	0,		'alpha', 0,		'offset', +pi/2)];		% polso3-ee:		theta polso pronosupinazione
+ 			Link('d', l_hand,	'a',	0,		'alpha', 0,		'offset', +pi/2)];		% polso3-ee:		theta polso pronosupinazione
 
 Left_Arm = SerialLink(Link_l, 'name', 'Left arm');
 Left_Arm.base = T01_left;
 %Left_Arm.tool = rt2tr(rotz(pi/2), [0; 0; l3_l]);
-
 
 % attenzione: il sistema di riferimento dell'ambiente 3D di Bot.plot e` il
 % sistema di riferimento del primo Link (che crede essere il telaio) A MENO
@@ -65,7 +66,36 @@ q0_l = zeros(1, Left_Arm.n);
 % 	Right_Arm.plot(q);
 % 	Left_Arm.plot(q);
 % end
+% 
+% Right_Arm.teach
+% figure
+% Left_Arm.teach
 
-Right_Arm.teach
-figure
-Left_Arm.teach
+ 
+%% inv kine?
+
+T0Hand_l = zeros(4, 4, size(trial.P_Hand_L,1));
+for i=1:size(trial.P_Hand_L,1)
+	T0Hand_l(:,:,i) = rt2tr(rotz(pi/2), trial.P_Hand_L(i,:)');
+end
+
+qtry = Left_Arm.ikunc(T0Hand_l);
+
+Left_Arm.plot(qtry)
+Right_Arm.plot(qtry)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Data Analysis/temp_quat_prove.m
@@ -0,0 +1,35 @@
+%%temp
+clear; clc; close
+load('healthy.mat');
+trial  = healthy_task(7).subject(1).right_side_trial(1);
+clear healthy_task;
+
+t = trial.P_Forearm_R;
+quat_s = quaternion(trial.P_Shoulder_R_Quat);
+quat_u = quaternion(trial.P_Upperarm_R_Quat);
+quat_f = quaternion(trial.P_Forearm_R_Quat);
+quat_h = quaternion(trial.P_Hand_R_Quat);
+for i=1:size(quat_s,1)
+	quat_hand(i,1) = quat_s(i) * quat_u(i) * quat_f(i) * quat_h(i);
+end
+
+for i=1:size(t,1)
+	grid on
+	axis equal
+	xlabel('x');    ylabel('y');    zlabel('z');
+	xlim([-1, 1]);  ylim([-1, 1]); zlim([0, 2]);
+	plotTransforms(t(i,:), quat_hand(i,:), 'Meshfile', 'mattone.stl')
+	drawnow()
+end
+%rosso X
+%verde Y
+%blu Z
+
+%% DUBBIO
+
+% se i quaternioni vanno impilati per ottenere la generica matrice di
+% trasformazione T0i, per loro, quale segmento ci fornisce 0? Dobbiamo poi
+% calcolare tutte le trasformazioni da li` alla mano?
+
+
+
