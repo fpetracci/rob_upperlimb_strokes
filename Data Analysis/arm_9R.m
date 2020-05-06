e915e220aabf42cdd64e228ace1f1fcd1bf4379e
AndreaFerroni <andreaferroni.1996@gmail.com>
2020-05-06 14:41:22 +0200

    load_mvnx.m updated without having the bar

--- Data Analysis/arm_9R.m
+++ Data Analysis/arm_9R.m
@@ -1,15 +1,9 @@
 %% Intro
 clear; clc; close;
-
-load('healthy_task.mat');
-trial = healthy_task(1).subject(1).left_side_trial(1);
-clear healthy_task;
- % trial = struct_dataload('H01_T07_L1.mvnx');
-
- 
-% sistema 
-
-
+tic
+% load('healthy.mat');
+% trial = healthy_task(1).subject(1).left_side_trial(1);
+% clear healthy_task;
 
 
 %% define link of our serial links manipulator
@@ -19,11 +13,10 @@ l_h = 0.15; 	% wrist - hand
 
 pos_pelvis = trial.Pelvis.Pos(1,:); %BARBATRUCCO
 %% Costruction, right arm
-
-T01_right = rt2tr(rotz(-pi/2), pos_pelvis');
+T01_right = rt2tr(eye(3), pos_pelvis');
 
 d1 = lengths.h_torso;
-d2 = -lengths.d_neck;
+d2 = lengths.d_neck;
 a2 = lengths.shoulder.right;
 a5 = lengths.upperarm.right;
 a6 = lengths.forearm.right;
@@ -44,9 +37,9 @@ Right_Arm = SerialLink(Link_r, 'name', 'Right arm');
 Right_Arm.base = T01_right;
 
 %% Costruction, left arm
-T01_left = rt2tr(rotz(-pi/2)*roty(pi), pos_pelvis');
+T01_left = rt2tr(roty(pi), pos_pelvis');
 
-d1 = -lengths.h_torso;
+d1 = lengths.h_torso;
 d2 = lengths.d_neck;
 a2 = lengths.shoulder.left;
 a5 = lengths.upperarm.left;
@@ -55,7 +48,7 @@ d9 = +l_h;
 
 
 % serial links connection
-Link_l = [	Link('d', d1,	'a',	0,		'alpha', -pi/2),...						% bacino-Neck':		theta torsione busto
+Link_l = [	Link('d', -d1,	'a',	0,		'alpha', -pi/2),...						% bacino-Neck':		theta torsione busto
 			Link('d', d2,	'a',	a2,		'alpha', 0),...							% Neck'-spalla1:	theta "alzata" spalla (abduzione?)
 			Link('d', 0,	'a',	0,		'alpha', +pi/2),...						% spalla1-spalla2:	theta spalla pronosupinazione
 			Link('d', 0,	'a',	0,		'alpha', +pi/2, 'offset', +pi/2),...	% spalla2-spalla3:	theta spalla spazzata orizzontale
@@ -74,11 +67,11 @@ Left_Arm.base = T01_left;
 
 %% plot
 q0 = zeros(1, Right_Arm.n);		%initial config
-q0 = Left_Arm.ikunc( rt2tr(quat2rotm(trial.Hand_L.Quat), trial.Hand_L.Pos') );
+q0 = Left_Arm.ikunc (rt2tr(quat2rotm(trial.Hand_L.Quat(:,:)),trial.Hand_L.Pos(:,:)'))
 Right_Arm.plot(q0)
 view([25 10 45])
 figure(2)
 Left_Arm.plot(q0)
 %% inv kine?
 
-
+toc
