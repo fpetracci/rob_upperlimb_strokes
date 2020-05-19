091d8efca5fa0de2c3f35ad3970299dca778012a
AndreaFerroni <andreaferroni.1996@gmail.com>
2020-05-19 13:31:50 +0200

    creation of two points translated in x and y for wrist, elbow and shoulder, as suggested by Giuseppino.

--- Data Analysis/new_Kalman_marker/fkine_kalman_marker.m
+++ Data Analysis/new_Kalman_marker/fkine_kalman_marker.m
@@ -1,4 +1,4 @@
-function yk = fkine_kalman(qk, Arm)
+function yk = fkine_kalman_marker(qk, Arm)
 % This function generates the measurement vector (21x1) for kalman filter.
 %
 % The measurement is the forward kinematics of the angular joints.
@@ -21,40 +21,22 @@ function yk = fkine_kalman(qk, Arm)
 % the 0-frame. Including it in the meas vector could lead into
 % computational errors.
 
-Tg3	= arm_fkine(Arm, qk, 3);
+Tg3	= arm_fkine(Arm, qk, 3)
 rot_L5 = Tg3(1:3, 1:3);
 eul_L5 = rotm2eul(rot_L5,'ZYZ')';
 
 %% shoulder
 % Tg3 = arm_fkine(Arm, qk, 3);
-tr_shoulder = Tg3(1:3, 4);
 
-Tg6	= arm_fkine(Arm, qk, 6);
-Tg6_newx =  Tg6 * ([1 0 0 0.05; 0 1 0 0; 0 0 1 0; 0 0 0 1]);
-Tg6_newy =	Tg6 * ([1 0 0 0; 0 1 0 0.05; 0 0 1 0; 0 0 0 1]) ;
+Tg6	= arm_fkine(Arm, qk, 6)
+Tg6_trasl_x =  [Tg6(:,1:3) Tg3(:,4)] * [eye(3) [0.05 0 0]'; 0 0 0 1]; % ho riportato Tg6 nella forma correttta orientazione di Tg6, posizione di Tg3
+Tg6_trasl_y =  [Tg6(:,1:3) Tg3(:,4)] * [eye(3) [0 0.05 0]'; 0 0 0 1];
 	
+plot_arrows(Tg6) %% non è proprio esatto perche la posizione è quella di Tg3
 
-    ox(1) = Tg6(1,4);
-    oy(1) = Tg6(2,4);
-    oz(1) = Tg6(3,4);
-    ux(1) = Tg6(1,1);
-    vx(1) = Tg6(2,1);
-    wx(1) = Tg6(3,1);
-    uy(1) = Tg6(1,2);
-    vy(1) = Tg6(2,2);
-    wy(1) = Tg6(3,2);
-    uz(1) = Tg6(1,3);
-    vz(1) = Tg6(2,3);
-    wz(1) = Tg6(3,3);
-
-    % Create graph
-	figure(1), hold on
-    quiver3(ox, oy, oz, ux, vx, wx,  'r', 'ShowArrowHead', 'on', 'MaxHeadSize', 0.999999, 'AutoScale', 'off');
-    quiver3(ox, oy, oz, uy, vy, wy,  'g', 'ShowArrowHead', 'on', 'MaxHeadSize', 0.999999, 'AutoScale', 'off');
-    quiver3(ox, oy, oz, uz, vz, wz,  'b', 'ShowArrowHead', 'on', 'MaxHeadSize', 0.999999, 'AutoScale', 'off');
-	plot3(Tg6_newx(1,4),Tg6_newx(2,4),Tg6_newx(3,4),'*r')
-	plot3(Tg6_newy(1,4),Tg6_newy(2,4),Tg6_newy(3,4),'*r')
-
+tr_shoulder = Tg3(1:3, 4);
+tr_shoulder_x = Tg6_trasl_x(1:3, 4);
+tr_shoulder_y = Tg6_trasl_y(1:3, 4);
 
 rot_shoulder = Tg6(1:3, 1:3);
 eul_shoulder = rotm2eul(rot_shoulder,'ZYZ')';
@@ -62,17 +44,29 @@ eul_shoulder = rotm2eul(rot_shoulder,'ZYZ')';
 
 %% elbow
 % Tg6 = arm_fkine(Arm, qk, 6);
-tr_elbow = Tg6(1:3, 4);
 
 Tg8	= arm_fkine(Arm, qk, 8);
+Tg8_trasl_x =  [Tg8(:,1:3) Tg6(:,4)] * [eye(3) [0.05 0 0]'; 0 0 0 1]; 
+Tg8_trasl_y =  [Tg8(:,1:3) Tg6(:,4)] * [eye(3) [0 0.05 0]'; 0 0 0 1];
+
+tr_elbow = Tg6(1:3, 4);
+tr_elbow_x = Tg8_trasl_x(1:3, 4);
+tr_elbow_y = Tg8_trasl_y(1:3, 4);
+
 rot_elbow = Tg8(1:3, 1:3);
 eul_elbow = rotm2eul(rot_elbow,'ZYZ')';
 
 %% wrist
 % Tg8 = arm_fkine(Arm, qk, 8);
-tr_wrist = Tg8(1:3, 4);
 
 Tg10 = arm_fkine(Arm, qk, 10);
+Tg10_trasl_x =  [Tg10(:,1:3) Tg8(:,4)] * [eye(3) [0.05 0 0]'; 0 0 0 1]; 
+Tg10_trasl_y =  [Tg10(:,1:3) Tg8(:,4)] * [eye(3) [0 0.05 0]'; 0 0 0 1];
+
+tr_wrist = Tg8(1:3, 4);
+tr_wrist_x = Tg10_trasl_x(1:3, 4);
+tr_wrist_y = Tg10_trasl_y(1:3, 4);
+
 rot_wrist = Tg10(1:3, 1:3);
 eul_wrist = rotm2eul(rot_wrist,'ZYZ')'; 
 
@@ -86,12 +80,14 @@ eul_wrist = rotm2eul(rot_wrist,'ZYZ')';
 % 		tr_wrist; ...
 % 		eul_wrist];
 
-yk = [	eul_L5; ...
-		tr_shoulder; ...
-		eul_shoulder; ...
+yk = [	tr_shoulder; ...
+		tr_shoulder_x; ...
+		tr_shoulder_y; ...
 		tr_elbow; ...
-		eul_elbow; ...
+		tr_elbow_x; ...
+		tr_elbow_y; ...
 		tr_wrist; ...
-		eul_wrist];
-
+		tr_wrist_x; ...
+		tr_wrist_y];
+		
 end
