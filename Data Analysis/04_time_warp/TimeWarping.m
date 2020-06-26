1b396a60a822c600c31e85281b55224577433dd1
AndreaFerroni <andreaferroni.1996@gmail.com>
2020-06-26 17:33:14 +0200

    TimeWarping

--- Data Analysis/04_time_warp/TimeWarping.m
+++ Data Analysis/04_time_warp/TimeWarping.m
@@ -3,8 +3,10 @@ function [s2_new] = TimeWarping(s1,s2)
 
 % s1 movimento di riferimento
 % s2 movimento da modificare
-
-%creazione parametri per modificare s2
+s1 = healthy_task_q(1).subject(1).left_side_trial(1).q_grad ;
+s2 = healthy_task_q(2).subject(1).left_side_trial(1).q_grad ;
+trial = s2;
+%creazione parametri per modificare s2 DA GUARDARE PER OTTIMIZZARE
 rapp = length(s2(1,:))/length(s1(1,:));
 tshtmp = round(length(s2(1,:))/5);
 TShift = round(linspace(-tshtmp,tshtmp,10));
@@ -22,7 +24,7 @@ for i=1:length(TShift(1,:))
         
         time = 1 : length(s2tmp(1,:));
         nel = length(time);
-        ts1 = timeseries(s2tmp(1,:),time); 
+        ts1 = timeseries(s2tmp(1,:),time); % 7 perchè è l'angolo scelto su cui fare il calcolo dei parametri
 %         ts2 = timeseries(s2tmp(2,:),time); 
 %         ts3 = timeseries(s2tmp(3,:),time); 
 %         ts4 = timeseries(s2tmp(4,:),time); 
@@ -134,33 +136,41 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%% Stretch %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 time = 1 : length(s2(1,:));
 nel = length(time);
-% YLE
-% ts1 = timeseries(s2(1,:),time); 
-% ts2 = timeseries(s2(2,:),time); 
-% ts3 = timeseries(s2(3,:),time); 
-% ts4 = timeseries(s2(4,:),time); 
-% ts5 = timeseries(s2(5,:),time); 
-% ts6 = timeseries(s2(6,:),time); 
-ts1 = timeseries(s2(1,:),time); %COMM
+ts1 = timeseries(s2(1,:),time); 
+ts2 = timeseries(s2(2,:),time); 
+ts3 = timeseries(s2(3,:),time); 
+ts4 = timeseries(s2(4,:),time); 
+ts5 = timeseries(s2(5,:),time); 
+ts6 = timeseries(s2(6,:),time); 
+ts7 = timeseries(s2(7,:),time); 
+ts8 = timeseries(s2(8,:),time); 
+ts9 = timeseries(s2(9,:),time); 
+ts10 = timeseries(s2(10,:),time); 
+
 %Nuovo asse temporale "stretchato"
 timenew = linspace(time(1),time(end),ceil(nel*NShap(I_col)));
 
-%YLE
-% ts1r = resample(ts1,timenew); 
-% ts2r = resample(ts2,timenew); 
-% ts3r = resample(ts3,timenew); 
-% ts4r = resample(ts4,timenew); 
-% ts5r = resample(ts5,timenew); 
-% ts6r = resample(ts6,timenew);
-ts1r = resample(ts1,timenew); % COMM
-% YLE
-% s2 = [ts1r.data(:)';...
-%       ts2r.data(:)';...
-%       ts3r.data(:)';...
-%       ts4r.data(:)';...
-%       ts5r.data(:)';...
-%       ts6r.data(:)'];
-s2 = [ts1r.data(:)']; % COMM
+ts1r = resample(ts1,timenew); 
+ts2r = resample(ts2,timenew); 
+ts3r = resample(ts3,timenew); 
+ts4r = resample(ts4,timenew); 
+ts5r = resample(ts5,timenew); 
+ts6r = resample(ts6,timenew);
+ts7r = resample(ts7,timenew); 
+ts8r = resample(ts8,timenew); 
+ts9r = resample(ts9,timenew);
+ts10r = resample(ts10,timenew);
+
+s2 = [ts1r.data(:)';...
+      ts2r.data(:)';...
+      ts3r.data(:)';...
+      ts4r.data(:)';...
+      ts5r.data(:)';...
+	  ts6r.data(:)';...
+      ts7r.data(:)';...
+      ts8r.data(:)';...
+	  ts9r.data(:)';...
+      ts10r.data(:)'];
 %%%%%%%%%%%%%%%%%%%%%%%%%% Time Shift %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if TShift(I_row) >= 0 
@@ -168,26 +178,34 @@ if TShift(I_row) >= 0
     % del primo frame in testa al vettore delle misure (ritarda il
     % segnale)
 	%YLE
-%     tmpv1 = [ones(1, TShift(I_row))*s2(1,1) s2(1,:)];
-%     tmpv2 = [ones(1, TShift(I_row))*s2(2,1) s2(2,:)];
-%     tmpv3 = [ones(1, TShift(I_row))*s2(3,1) s2(3,:)];
-%     tmpv4 = [ones(1, TShift(I_row))*s2(4,1) s2(4,:)];
-%     tmpv5 = [ones(1, TShift(I_row))*s2(5,1) s2(5,:)];
-%     tmpv6 = [ones(1, TShift(I_row))*s2(6,1) s2(6,:)];
- tmpv1 = [ones(1, TShift(I_row))*s2(1,1) s2(1,:)]; % COMM
-%     s2 = [tmpv1; tmpv2; tmpv3; tmpv4; tmpv5; tmpv6];
-	s2 = [tmpv1];
+    tmpv1 =  [ones(1, TShift(I_row))*s2(1,1) s2(1,:)];
+    tmpv2 =  [ones(1, TShift(I_row))*s2(2,1) s2(2,:)];
+    tmpv3 =  [ones(1, TShift(I_row))*s2(3,1) s2(3,:)];
+    tmpv4 =  [ones(1, TShift(I_row))*s2(4,1) s2(4,:)];
+    tmpv5 =  [ones(1, TShift(I_row))*s2(5,1) s2(5,:)];
+    tmpv6 =  [ones(1, TShift(I_row))*s2(6,1) s2(6,:)];
+	tmpv7 =  [ones(1, TShift(I_row))*s2(7,1) s2(7,:)];
+    tmpv8 =  [ones(1, TShift(I_row))*s2(8,1) s2(8,:)];
+    tmpv9 =  [ones(1, TShift(I_row))*s2(9,1) s2(9,:)];
+	tmpv10 = [ones(1, TShift(I_row))*s2(10,1) s2(10,:)];
+
+
+     s2 = [tmpv1; tmpv2; tmpv3; tmpv4; tmpv5; tmpv6;...
+			tmpv7; tmpv8; tmpv9; tmpv10];
+
 elseif TShift(I_row) < 0
     % Con shift negativo taglia i primi n=TShift(i) campioni dalla
     % misura
-    % YLE
-% 	s2 = [s2(1,abs(TShift(I_row)):end);...
-%           s2(2,abs(TShift(I_row)):end);...
-%           s2(3,abs(TShift(I_row)):end);...
-%           s2(4,abs(TShift(I_row)):end);...
-%           s2(5,abs(TShift(I_row)):end);...
-%           s2(6,abs(TShift(I_row)):end)];
-	s2 = [s2(1,abs(TShift(I_row)):end)];
+	s2 = [s2(1,abs(TShift(I_row)):end);...
+          s2(2,abs(TShift(I_row)):end);...
+          s2(3,abs(TShift(I_row)):end);...
+          s2(4,abs(TShift(I_row)):end);...
+          s2(5,abs(TShift(I_row)):end);...
+          s2(6,abs(TShift(I_row)):end);...
+		  s2(7,abs(TShift(I_row)):end);...
+          s2(8,abs(TShift(I_row)):end);...
+		  s2(9,abs(TShift(I_row)):end);...
+		  s2(10,abs(TShift(I_row)):end)];
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%% Complete %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -199,29 +217,39 @@ Ls2 = length(s2(1,:));
 if Ls1 > Ls2
     %Allunga s2 copiando in coda Ls1-Ls2 volte l'ultimo valore del
     %segnale
-	% YLE
-%     tmpv1 = [s2(1,:) ones(1,Ls1-Ls2)*s2(1,end)]; 
-%     tmpv2 = [s2(2,:) ones(1,Ls1-Ls2)*s2(2,end)]; 
-%     tmpv3 = [s2(3,:) ones(1,Ls1-Ls2)*s2(3,end)]; 
-%     tmpv4 = [s2(4,:) ones(1,Ls1-Ls2)*s2(4,end)]; 
-%     tmpv5 = [s2(5,:) ones(1,Ls1-Ls2)*s2(5,end)]; 
-%     tmpv6 = [s2(6,:) ones(1,Ls1-Ls2)*s2(6,end)];
-	tmpv1 = [s2(1,:) ones(1,Ls1-Ls2)*s2(1,end)];
-%    s2 = [tmpv1; tmpv2; tmpv3; tmpv4; tmpv5; tmpv6];
-	s2 = [tmpv1];
+    tmpv1 = [s2(1,:) ones(1,Ls1-Ls2)*s2(1,end)]; 
+    tmpv2 = [s2(2,:) ones(1,Ls1-Ls2)*s2(2,end)]; 
+    tmpv3 = [s2(3,:) ones(1,Ls1-Ls2)*s2(3,end)]; 
+    tmpv4 = [s2(4,:) ones(1,Ls1-Ls2)*s2(4,end)]; 
+    tmpv5 = [s2(5,:) ones(1,Ls1-Ls2)*s2(5,end)]; 
+    tmpv6 = [s2(6,:) ones(1,Ls1-Ls2)*s2(6,end)];
+	tmpv7 = [s2(7,:) ones(1,Ls1-Ls2)*s2(7,end)]; 
+    tmpv8 = [s2(8,:) ones(1,Ls1-Ls2)*s2(8,end)]; 
+    tmpv9 = [s2(9,:) ones(1,Ls1-Ls2)*s2(9,end)];
+	tmpv10 = [s2(10,:) ones(1,Ls1-Ls2)*s2(10,end)];
+
+   s2 = [tmpv1; tmpv2; tmpv3; tmpv4; tmpv5; tmpv6;...
+		 tmpv7; tmpv8; tmpv9; tmpv10];
 elseif Ls1 <Ls2
     %Taglia gli ultimi valori di s2 per arrivare alla lunghezza di s1
-	%YLE
-%     s2 = [s2(1,1:Ls1);...
-%           s2(2,1:Ls1);...
-%           s2(3,1:Ls1);...
-%           s2(4,1:Ls1);...
-%           s2(5,1:Ls1);...
-%           s2(6,1:Ls1)];
-	s2 = [s2(1,1:Ls1)];
+    s2 = [s2(1,1:Ls1);...
+          s2(2,1:Ls1);...
+          s2(3,1:Ls1);...
+          s2(4,1:Ls1);...
+          s2(5,1:Ls1);...
+		  s2(6,1:Ls1);...
+          s2(7,1:Ls1);...
+          s2(8,1:Ls1);...
+          s2(9,1:Ls1);...
+          s2(10,1:Ls1)];
 end
 
 %Salva s2 modificato nell'output
 s2_new = s2;
-
+figure(1)
+plot(s1')
+figure(2)
+plot(s2_new')
+figure(3)
+plot(trial')
 end
