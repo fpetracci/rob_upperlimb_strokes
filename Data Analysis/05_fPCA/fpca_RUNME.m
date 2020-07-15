31e65247f5232a9f26f1d4d94ad35bedf5f91ab4
fpetracci <petracci.francesco@gmail.com>
2020-07-15 15:36:51 +0200

    anovan test

--- Data Analysis/05_fPCA/fpca_RUNME.m
+++ Data Analysis/05_fPCA/fpca_RUNME.m
@@ -64,7 +64,7 @@ end
 
 clearvars -except q_fpca_task_s q_fpca_task_h fPCA_task q_stacked_task
 
-%% plots
+%% plots 1 obs h
 close all
 task_chosen = 1;
 obs = 1;
@@ -152,3 +152,123 @@ for j = 1:10
 
 
 end
+%% plots 1 obs s
+task_chosen = 1;
+obs = 1;
+
+% for j = 1:10
+% 	figure(j)
+% 	for i = 1:4
+% 		subplot(2,4,i)
+% 		plot(q_fpca_task_h(task_chosen).joint(j).qmat(:,obs,i))
+% 		hold on
+% 		q_stacked_plot = q_stacked_task(task_chosen).q_matrix_h(:,:,j)';
+% 		plot(q_stacked_plot(:,obs))
+% 		title(['Joint number ' num2str(j)]);
+% 
+% 		subplot(2,4,i+4)
+% 		plot(q_fpca_task_h(task_chosen).joint(j).qmat_separated(:,obs,i))
+% 		hold on
+% 		q_stacked_plot = q_stacked_task(task_chosen).q_matrix_h(:,:,j)';
+% 		plot(q_stacked_plot(:,obs))
+% 		title(['Joint number ' num2str(j)]);
+% 
+% 	end
+% end
+
+for j = (10+1):(10+10)
+		figure(j)
+		j = j-10;
+		%figure('Renderer', 'painters', 'Position', [10 10 1900 1000])
+		clf
+		
+		% plot of angular joint
+		subplot(1,2,1)
+		plot(q_fpca_task_s(task_chosen).joint(j).qmat(:,obs,1),...
+			 'Linewidth',0.5,'DisplayName','Mean of Signal')
+		hold on
+		plot(q_fpca_task_s(task_chosen).joint(j).qmat(:,obs,2),...
+			 'Linewidth',0.5,'DisplayName','Mean + fPC1')
+		 plot(q_fpca_task_s(task_chosen).joint(j).qmat(:,obs,3),...
+			 'Linewidth',0.5,'DisplayName','Mean + fPC1 + fPC2')
+		 plot(q_fpca_task_s(task_chosen).joint(j).qmat(:,obs,4),...
+			 'Linewidth',0.5,'DisplayName','Mean + fPC1 + fPC2 + fPC3')
+		 
+		q_stacked_plot = q_stacked_task(task_chosen).q_matrix_h(:,:,j)';
+		plot(q_stacked_plot(:,obs), ...
+			'k--', 'Linewidth',1.5,'DisplayName','Actual Signal')
+		legend('Location','best')
+		xlabel('Time samples')
+		ylabel(' Angular Value [grad]')
+		xlim([1 length(q_fpca_task_s(task_chosen).joint(j).qmat(:,obs,1))])
+		grid on
+		title(['Joint number ' num2str(j)]);
+		
+		fontsize = 12;
+		set(gca,'FontSize',fontsize)
+		set(findall(gcf,'type','text'),'FontSize',fontsize)
+		hold off	
+		
+		
+		
+		% pareto
+		subplot(1,2,2)
+		var_list = fPCA_task(task_chosen).s_joint(j).var;
+		pareto2(var_list);
+		%hold on, %errorbar(mean(var_list),std(var_list))
+		title('Variance Explanations')
+		lgd = legend({'Variance','Cumulative Variance'},'FontSize', fontsize,'TextColor','black', 'Location', 'east');
+		%lgd.Position = [0.5 0.5 0.1433 0.1560];
+
+		%legend('Variance', 'Cumulative Variance')
+		axis([0 (length(var_list)+1) 0 1])
+		xlabel('fPC')
+		ylabel('Explained Variance')
+		xticks([1:length(var_list)])
+		yticks([0:0.1:1])
+		xticklabels([{'1'},{'2'},{'3'},{'4'},{'5'}])
+		%yticklabels([{'0%'},{'10%'},{'20%'},{'30%'},{'40%'},{'50%'},{'60%'},{'70%'},{'80%'},{'90%'},{'100%'}])
+		grid on
+		
+
+% 		subplot(1,2,i+4)
+% 		plot(q_fpca_task_h(task_chosen).joint(j).qmat_separated(:,obs,i))
+% 		hold on
+% 		q_stacked_plot = q_stacked_task(task_chosen).q_matrix_h(:,:,j)';
+% 		plot(q_stacked_plot(:,obs))
+% 		title(['Joint number ' num2str(j)]);
+
+
+end
+%
+
+%% anovan
+
+var_vect_pc1 = zeros(30*10*2,1);
+var_sum_h = zeros(10,1);
+var_sum_s = zeros(10,1);
+for i = 1:30
+	ii = (i-1)*10;
+	for j = 1:10
+		var_tmp_h = fPCA_task(i).h_joint(j).var(1);
+		var_tmp_s = fPCA_task(i).s_joint(j).var(1);
+		var_vect_pc1(ii+j) = var_tmp_h;
+		var_vect_pc1(30*10+ii+j) = var_tmp_s;
+		var_sum_h(j) = var_sum_h(j) + fPCA_task(i).h_joint(j).var(1);
+		var_sum_s(j) = var_sum_s(j) + fPCA_task(i).s_joint(j).var(1);
+	end
+end
+var_sum_h = var_sum_h/30;
+var_sum_s = var_sum_s/30;
+var_sum = [var_sum_h ;var_sum_h];
+%g1 = [ones(10,1); zeros(10*29+10*30,1)]; % healthy
+%g2 = [zeros(30*10,1); ones(10,1); zeros(10*29,1)]; % stroke
+
+% g1 = [ones(30*10,1); zeros(30*10,1)]; % healthy
+% g2 = [3*ones(30*10,1); 2*ones(30*10,1)]; % stroke
+% p = anovan(var_vect_pc1,{g1 g2})
+
+g1 = [ones(10,1); zeros(10,1)]; % healthy
+g2 = [zeros(10,1); ones(10,1)]; % stroke
+[a,b] = ttest((g1.*var_sum)',(g2.*var_sum)');
+p1 = anovan(var_sum,{g1 g2});
