d25b536682e6d3a647883c3ef05053ed8d23771d
fpetracci <petracci.francesco@gmail.com>
2020-12-04 13:05:55 +0100

    mini commit for franka visuals

--- Tavole/FRANKA/franka_control.m
+++ Tavole/FRANKA/franka_control.m
@@ -3,12 +3,24 @@ clear all ; clc ;close all
 
 mdl_franka 
 
-white = [1, 1, 1];
+light_grey = [0.99, 0.99, 0.99];
 black = [0, 0, 0];
 qv1 = qv;
 qv1(6) = pi/2;
+
+figure(1)
+
 franka.plotopt = {'workspace',[-0.75,0.75,-0.75,0.75,0,1]};
-franka.plot(qv1,'floorlevel',0,'linkcolor',white,'jointcolor',black)
+franka.plot(qv1,'floorlevel',0,'linkcolor',light_grey,'jointcolor',black)
+% lims  plot
+xlim([-1 1])
+ylim([-1 1])
+zlim([0 2])
+xlabel('X [m]')
+ylabel('Y [m]')
+zlabel('Z [m]')
+view(30,30)
+   
 %% Setup
 t_in = 0; % [s]
 t_fin = 10; % [s]
@@ -33,12 +45,12 @@ pos1 = franka.fkine(qv1);
 % lascio jtraj, ma non vi è alcuna menzione del jacobiano. Per questo credo
 % sia meglio utilizzare ctraj, in modo da ottenere una traiettoria in cui
 % la velocità ha andamento trapezoidale e subito dopo inversione cinematica 
-% per ottenere gli angoli di giunot. Si veda libro siciliano pag 168
+% per ottenere gli angoli di giunto. Si veda libro siciliano pag 168
  [q,qd,qdd] = jtraj(qv, qv1, n);
 % tc = ctraj(pos0, pos1, n);
 r = 0.1; % raggio del cerchio 
 for i=1:n
-	franka.plot(q(i,:),'floorlevel',0,'linkcolor',white,'jointcolor',black)
+	franka.plot(q(i,:),'floorlevel',0,'linkcolor',light_grey,'jointcolor',black)
 end
 %% a seguito del raggiungimento punto pos1 eseguo un cerchio di raggio r
 center = pos1(1:3,4) - [r;0;0];% partendo dal punto pos1 calcolo il cechio nel pianonel piano XZ
@@ -53,7 +65,7 @@ q_des = generate_trajectory(xi,qv1,franka);
 dq_des = gradient(q_des)*1000;
 ddq_des = gradient(dq_des)*1000;
 for i=1:length(t)
-	franka.plot(q_des(:,i)','floorlevel',0,'linkcolor',white,'jointcolor',black)
+	franka.plot(q_des(:,i)','floorlevel',0,'linkcolor',light_grey,'jointcolor',black)
 	hold on
 	plot3(x,y,z,'k','Linewidth',1.5)
 end
