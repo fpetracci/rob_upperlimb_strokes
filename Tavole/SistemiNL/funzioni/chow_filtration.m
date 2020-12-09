f43ccaca688fde01af194f7855a273ae78599a5f
AndreaFerroni <andreaferroni.1996@gmail.com>
2020-12-09 23:21:10 +0100

    accesibility unicycle and bicycle

--- Tavole/SistemiNL/funzioni/chow_filtration.m
+++ Tavole/SistemiNL/funzioni/chow_filtration.m
@@ -6,7 +6,7 @@ Dfull = D0;
 Dfull_1 = [];
 
 disp('D0 ='); disp(D0);
-while (rank(Dfull) < size(x,1)) && (rank(Dfull) ~= rank(Dfull_1))
+while (rank(Dfull) < size(x,2)) && (rank(Dfull) ~= rank(Dfull_1))% controllare
     Dfull_1 = Dfull;
     Dfull = [Dfull lie_bracket_dist(Dfull, D, x)];
     Dfull = lincols(Dfull);
