c4bcb962fc502e0d8e44da0ff488fe612c68a578
fpetracci <petracci.francesco@gmail.com>
2020-12-09 23:38:29 +0100

    git ignore in folder "Tavole"

--- Tavole/SistemiNL/startup_NL.m
+++ Tavole/SistemiNL/startup_NL.m
@@ -1,4 +1,4 @@
-disp('Franka model loading: Alessia Biondi, Andrea Ferroni and Francesco Petracci')
+disp('Non Linear model loading: Alessia Biondi, Andrea Ferroni and Francesco Petracci')
 
 if verLessThan('matlab', '7.0')
     warning('You are running a very old (and unsupported) version of MATLAB.  You will very likely encounter significant problems using this files but you are on your own with this');
@@ -6,6 +6,6 @@ end
 
 script_path = fileparts( mfilename('fullpath') );
 
-addpath(genpath(fullfile(script_path, 'Funzioni')));
+addpath(genpath(fullfile(script_path, 'funzioni')));
 
 clear script_path
