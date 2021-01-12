eac0f4f58f887587dc12f740101c0907d535fca3
AndreaFerroni <andreaferroni.1996@gmail.com>
2021-01-12 11:21:57 +0100

    cycle done

--- Tavole/SistemiNL/Simulink Uniciclo con clotoidi/init_uniciclo.m
+++ Tavole/SistemiNL/Simulink Uniciclo con clotoidi/init_uniciclo.m
@@ -7,13 +7,13 @@ disp('Caricamento Uniciclo')
 
 %% scelta percorso
 
-tipo_perc = input(' Scegli un percorso da far fare all''uniciclo: \n 1: Raggiungimento di un punto \n 2: Circonferenza di raggio fissato \n 3: Percorso con clotoidi \n... ');
+tipo_perc = input(' Scegli un percorso da far fare all''uniciclo: \n 1: Raggiungimento di un punto \n 2: Circonferenza di raggio fissato \n 3: Percorso con clotoidi \n 4: Retta passante origine \n... ');
 
 % Simulink Variant settings
 Punto			= Simulink.Variant('tipo_perc == 1');
 Circonferenza	= Simulink.Variant('tipo_perc == 2');
 Clotoidi		= Simulink.Variant('tipo_perc == 3');
-
+Retta			= Simulink.Variant('tipo_perc == 4');
 %% parametri simulazione
 
 t_end		= 20;		% simulation time
@@ -43,14 +43,14 @@ end
 
 v_iniziale		= 1;	% velocita` piano iniziale [m\s]
 theta_iniziale	= pi/3;	% angolo iniziale [rad]
-v_rif			= 5;	% velocità di riferimento lungo la traiettoria curvilinea [m/s]
+v_rif			= 3;	% velocità di riferimento lungo la traiettoria curvilinea [m/s]
 %% parametri di saturazione
 v_dot_sat = 5;			% saturazione controllo sull'accelerazione lineare[m/s^2]
 w_sat = 3*pi;				% saturazione controllo sulla velocità angolare[rad/s]
 %% parametri controllo
 
-K1 = [10; 10];	% error on x_p, y_p
-K2 = [20; 20];	% error on x_p dot, y_p dot
+% K1 = [10; 10];	% error on x_p, y_p
+% K2 = [20; 20];	% error on x_p dot, y_p dot
 
 %% PERCORSI
 switch tipo_perc
@@ -88,6 +88,9 @@ switch tipo_perc
 	case 2
 		figure(1)
 		clf
+		% controlli cerchio
+		K1 = [40; 40];	% error on x_p, y_p
+		K2 = [50; 50];	% error on x_p dot, y_p dot
 		l_ginput = 30; 
 		h_ginput = 20;
 		plot(x_p_iniziale, y_p_iniziale, 'b*', 'DisplayName', 'Punto Iniziale' )
@@ -103,12 +106,18 @@ switch tipo_perc
 		else
 			centro	= [points(1); points(2)];	% posizione del centro [m]
 		end
-		
+		if v_rif > 1
+			v_rif = 1;
+		end
+		v_r = v_rif; % raggio dipende dalla velocità di rif
 	%% Clotoidi
 	case 3
 	%%%   ginput
 	figure(1)
 	clf
+	% controlli raggiungimento clotoide
+	K1 = [40; 40];	% error on x_p, y_p
+	K2 = [50; 50];	% error on x_p dot, y_p dot
 	l_ginput = 30; 
 	h_ginput = 20;
 
@@ -120,10 +129,11 @@ switch tipo_perc
 	title('Clickare sui punti che si vuole raggiungere poi premere invio')
 	points = ginput;
 	%%% Points
+	points = [x_p_iniziale, y_p_iniziale ; points];
 	n_p = length(points);    %numero di punti
 	xp  = points(:,1);
 	yp  = points(:,2);
-
+	
 	npts = 400;             %risoluzione
 	theta_rette = zeros(n_p-1,1);
 	for i= 1:(n_p-1)        %creazione angoli per le rette
@@ -135,13 +145,13 @@ switch tipo_perc
 	   theta_rette=[theta_rette ; theta_rette(n_p-2)+pi];  %aggiungo theta finale casi dispari
 	end
 
-	L=[];               %Vettore con la lunghezza dei tratti
+	Lenghts=[];               %Vettore con la lunghezza dei tratti
 	Lsofar=[];          %Vettore con le lunghezze dei tratti cumulative
 
 	for i=1:(n_p-1)			%Creazione Punti percorso e indexclot
 		if  mod(i,2)==1         %dispari, tratti rettilinei
-			L= [L ; ( (yp(i+1)-yp(i))^2 + (xp(i+1)-xp(i))^2 )^0.5];
-			Lsofar=[Lsofar; sum(L)]; 
+			Lenghts= [Lenghts ; ( (yp(i+1)-yp(i))^2 + (xp(i+1)-xp(i))^2 )^0.5];
+			Lsofar=[Lsofar; sum(Lenghts)]; 
 		elseif mod(i,2)==0      %pari, tratti clotodei
 			[S0,S1,SM,SG,iter] = buildClothoid3arcG2( xp(i), yp(i), theta_rette(i-1), 0, xp(i+1), yp(i+1), theta_rette(i+1), 0);
 			indexclot(i/2).clot.S0= S0;
@@ -151,13 +161,18 @@ switch tipo_perc
 			Lsofarclot(i/2).Lclot(2)= indexclot(i/2).clot.S0.L+indexclot(i/2).clot.SM.L;
 
 			lclot=indexclot(i/2).clot.S0.L+indexclot(i/2).clot.SM.L+indexclot(i/2).clot.S1.L;         
-			L= [L ; lclot];
-			Lsofar=[Lsofar; sum(L)];
+			Lenghts= [Lenghts ; lclot];
+			Lsofar=[Lsofar; sum(Lenghts)];
 		end
 
 	end
 
-	Ltot=sum(L); %Lunghezza totale percorso
+	Ltot=sum(Lenghts); %Lunghezza totale percorso
+	%% Retta
+	case 4 
+		dir_retta = [cos(pi/4); sin(pi/4)];
+		K1 = [40; 40];	% error on x_p, y_p
+		K2 = [50; 50];	% error on x_p dot, y_p dot
 
 end
 
