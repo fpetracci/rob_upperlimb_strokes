b2af079eb717e916e008063eb9121bd704bb0495
f.petracci <petracci.francesco@gmail.com>
2021-01-13 11:33:04 +0100

    update rif animation

--- Tavole/SistemiNL/funzioni_controllo/gru_gen_movie.m
+++ Tavole/SistemiNL/funzioni_controllo/gru_gen_movie.m
@@ -14,7 +14,7 @@ function gru_gen_movie(out, flag_rel, rep, nfig, movie_mode, movie_title, spins,
 %							angles
 %			View		- initial view angles 
 %			
-%
+%Example: gru_gen_movie(out, 0, 1, 1, 0, 'ciao', [0,0])
 
 %% check input
 if nargin < 3
@@ -64,7 +64,7 @@ end
 %% animation parameters
 
 % how many frames we want to skip
-fr_skip = 100;
+fr_skip = 30;
 
 % pause time in the animation
 time_pause = 0;
@@ -75,7 +75,7 @@ movie_fps = 20;		% movie frame per second
 
 % trajectory
 col_traj	= [230, 153, 0]./255;
-width_traj	= 2;
+width_traj	= 0.5;
 
 %% animation&movie
 
@@ -90,22 +90,25 @@ grid on
 for i = 1:fr_skip:nsteps
 	if i ~= 1
 		% delete last step stuff
-		delete(traj)
-		delete(p1)
-		delete(p2)
-		delete(p3)
+% 		delete(traj)
+% 		delete(p1)
+% 		delete(p2)
+% 		delete(p3)
+% 		delete(p4)
+% 		delete(p5)
+		hold off
+		
 	end
 	
-	
 	% gru plot
-	[p1, p2, p3] = draw_gru(out, i, flag_rel, nfig);
+	[p1, p2, p3, p4, p5] = draw_gru(out, i, flag_rel, nfig);
 	
 
 	% trajectory "so far" of ball
 	traj =	plot3(  ball_traj(1:i,1), ball_traj(1:i,2), -ball_traj(1:i,3),...
-            '--','color', col_traj, 'LineWidth', width_traj);
+            '--','color', col_traj, 'LineWidth', width_traj, 'DisplayName', 'Traiettoria');
 
-%----------------------------------------------------------------------
+	%----------------------------------------------------------------------
 	% lims  plot
 	[xlims, ylims, zlims] = get_lims(ball_traj);
 	xlim(xlims)
@@ -113,11 +116,14 @@ for i = 1:fr_skip:nsteps
 	zlim(zlims)
 	xlabel('X [m]')
 	ylabel('Y [m]')
-	zlabel('Z [m]')
-
+	zlabel('-Z [m]')
+	title(['Animazione, time: ' num2str(time(i)) ' di ' num2str(time(end))])
 	%----------------------------------------------------------------------
 	% end stuff
 
+	grid on
+	%legend % slow down animation too much
+	
 	% maximize window
 	if i == 1 && movie_mode == 2
 		figh.WindowState = 'maximize';
@@ -170,7 +176,7 @@ end
 end
 
 %% Extra Functions
-function [p1, p2, p3] = draw_gru(out, i, flag_rel, nfig)
+function [p1, p2, p3, p4, p5] = draw_gru(out, i, flag_rel, nfig)
 % out			- simulink output
 % i				- i.th timeframe
 % flag_rel		- if 1 pos_ball relativa
@@ -194,28 +200,62 @@ x_b			= out.pos_ball.Data(i,1);
 y_b			= out.pos_ball.Data(i,2);
 z_b			= out.pos_ball.Data(i,3);
 
+%riferimento palla
+if size(out.rif_ball.Data(:,1),1) == 1
+	x_b_rif		= out.rif_ball.Data(1,1);
+	y_b_rif		= out.rif_ball.Data(1,2);
+	z_b_rif		= out.rif_ball.Data(1,3);
+else
+	x_b_rif		= out.rif_ball.Data(i,1);
+	y_b_rif		= out.rif_ball.Data(i,2);
+	z_b_rif		= out.rif_ball.Data(i,3);
+end
+
+%riferimento palla interno
+x_b_rif_rel		= out.rif_interno.Data(i,1);
+y_b_rif_rel		= out.rif_interno.Data(i,2);
+z_b_rif_rel		= out.rif_interno.Data(i,3);
+
 % plot relativo o meno
 if flag_rel == 1
 	x_b = x_b - x_t;
 	y_b = y_b - y_t;
 	z_b = z_b - z_t;
 	
+	%riferimento palla
+	x_b_rif		= out.rif_ball.Data(i,1) - x_t;
+	y_b_rif		= out.rif_ball.Data(i,2) - y_t;
+	z_b_rif		= out.rif_ball.Data(i,3) - z_t;
+	
 	x_t = 0;
 	y_t = 0;
 	z_t = 0;
+	
+else
+	x_b_rif_rel = x_b_rif_rel + x_t;
+	y_b_rif_rel = y_b_rif_rel + y_t;
+	z_b_rif_rel = z_b_rif_rel + z_t;	
 end
 
 %% parametri plots
+dim_rif = 5;
+dim_ball = 10;
+dim_cart = 5;
+
+width_asta = 1.2;
 
 %% plots
 figure(nfig)
 % asta
-p1 = plot3([x_b; x_t], [y_b; y_t], -[z_b; z_t], 'k-', 'LineWidth', 2);
+p1 = plot3([x_b; x_t], [y_b; y_t], -[z_b; z_t], 'k-', 'LineWidth', width_asta, 'DisplayName', 'Asta');
 hold on
+% riferimenti
+p2 = plot3(x_b_rif, y_b_rif, -z_b_rif, 'o', 'Color', [0, 68, 204]/255, 'Markersize', dim_rif, 'DisplayName', 'Rif Palla');
+p3 = plot3(x_b_rif_rel, y_b_rif_rel, -z_b_rif_rel, 'o', 'Color', [153,187,255]/255, 'Markersize', dim_rif, 'DisplayName', 'Rif Interno');
 % palla
-p2 = plot3(x_b, y_b, -z_b, 'bo', 'Markersize', 10);
+p4 = plot3(x_b, y_b, -z_b, 'bo', 'Markersize', dim_ball, 'DisplayName', 'Palla');
 % cart
-p3 = plot3(x_t, y_t, -z_t, 'rd', 'Markersize', 5);
+p5 = plot3(x_t, y_t, -z_t, 'rd', 'Markersize', dim_cart, 'DisplayName', 'Cart');
 
 
 
