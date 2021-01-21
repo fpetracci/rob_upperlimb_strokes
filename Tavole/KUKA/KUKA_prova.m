72d8cdd10eb7fdddaa9dfda38a720b50611c8dd6
AndreaFerroni <andreaferroni.1996@gmail.com>
2021-01-21 23:32:29 +0100

    adaptive BS

--- Tavole/KUKA/KUKA_prova.m
+++ Tavole/KUKA/KUKA_prova.m
@@ -160,10 +160,6 @@ end
 % Gain circumference parameters matrix
 Kp = 20*diag([3 3 3 3 5 ]);
 Kv = 10*diag([1 1 1 1 1]);
-
-% Good Helix parameters matrix
-% Kp = 200*diag([3 3 3 3 5 3 5]);
-% Kv = 25*diag([1 1 1 1 70 2 70]);
 %% Computed torque with correct estimation
 results_computed_torque1 = q0;
 index = 1;
@@ -356,11 +352,16 @@ end
 KUKA.plot(q_des(:,1:50:end)','trail','black')
 	case 3
 %% ---------------------BACKSTEPPING_ADAPTIVE------------------------------
-%% Trajectory Tracking:Backstepping Method
-n = 5;
-for j = 1:n 
-    KUKAmodel.links(j).m = KUKAmodel.links(j).m .* (1.1); 
-end
+%% Perturbazione iniziale dei parametri
+	q_des = q_des';
+	dq_des  = dq_des';
+	ddq_des = ddq_des';
+	int = 2.5; % intensità percentuale della perturbazione sui parametri
+	n = 5;
+	for j = 1:n 
+		KUKAmodel.links(j).m = KUKAmodel.links(j).m .* (1+int/100); 
+	end
+%% Simulazione
 q = zeros(length(t),n); 
 q_dot = zeros(length(t),n); 
 tau = zeros(length(t),n); 
@@ -372,114 +373,98 @@ q_dot(1,:) = q_dot0;
 qr_dot = zeros(length(t),n); 
 qr_ddot = zeros(length(t),n); 
 
-pi0 = zeros(1,n*10);
+pi0 = zeros(1,n*10); 
 for j = 1:n
     pi0((j-1)*10+1:j*10) = [KUKAmodel.links(j).m KUKAmodel.links(j).m*KUKAmodel.links(j).r ...
         KUKAmodel.links(j).I(1,1) 0 0 KUKAmodel.links(j).I(2,2) 0 KUKAmodel.links(j).I(3,3)];
 end
-n = 5;
-piArray(1,:) = pi0;
-for j = 1:n 
-    KUKAmodel.links(j).m = KUKAmodel.links(j).m .* (1.1); 
-end
-% Gain circumference parameters matrix
-Kd = 0.1*diag([200 200 200 20 1]);
+piArray(1,:) = pi0; 
+
 Kp = 1*diag([200 200 200 20 10]);
+Kv = 0.1*diag([200 200 200 10 1]); 
+Kd = 0.1*diag([200 200 200 20 1]);
+
 % P e R fanno parte della candidata di Lyapunov, quindi devono essere definite positive
 R = diag(repmat([1e1 repmat(1e3,1,3) 1e2 1e7 1e7 1e2 1e7 1e2],1,n)); 
 P = 0.01*eye(10);
 lambda = diag([200, 200, 200, 200, 200])*0.03;
-%% calcolo q
-% for i = 2:length(t)
-    %% Interruzione della simulazione se q diverge
+
+
+
+tic
+for i = 2:length(t)
+%% Interruzione della simulazione se q diverge
     if any(isnan(q(i-1,:)))
         fprintf('Simulazione interrotta! \n')
         return
+	end
+	
+%% Calcolo dell'errore: e, e_dot
+    e = q_des(i-1,:) - q(i-1,:); 
+    e_dot = dq_des(i-1,:) - q_dot(i-1,:); 
+    s = (e_dot + e*lambda);
+    
+    qr_dot(i-1,:) = dq_des(i-1,:) + e*lambda;
+    if (i > 2)
+        qr_ddot(i-1,:) = (qr_dot(i-1) - qr_dot(i-2)) / delta_t;
+	end
+	
+	for j = 1:n 
+		KUKAmodel.links(j).m = piArray(i-1,(j-1)*10+1); % elemento 1 di pi
+	end
+	Mtilde = KUKAmodel.inertia(q(i-1,:)); 
+    Ctilde = KUKAmodel.coriolis(q(i-1,:),q_dot(i-1,:)); 
+    Gtilde = KUKAmodel.gravload(q(i-1,:)); 
+	tau(i,:) = qr_ddot(i-1,:)*Mtilde' + qr_dot(i-1,:)*Ctilde' + Gtilde + s*Kd' + e*Kp';
+	%% Dinamica del manipolatore (reale)
+    % entrano tau, q e q_dot, devo calcolare M, C e G e ricavare q_ddot
+    % integro q_ddot due volte e ricavo q e q_dot
+    M = KUKA.inertia(q(i-1,:)); 
+    C = KUKA.coriolis(q(i-1,:),q_dot(i-1,:)); 
+    G = KUKA.gravload(q(i-1,:)); 
+    
+    q_ddot = (tau(i,:) - q_dot(i-1,:)*C' - G) * (M')^(-1); 
+    
+    q_dot(i,:) = q_dot(i-1,:) + delta_t*q_ddot; 
+    q(i,:) = q(i-1,:) + delta_t*q_dot(i,:);
+	 %% Dinamica dei parametri
+	q1 = q(i,1); q2 = q(i,2); q3 = q(i,3); q4 = q(i,4); q5 = q(i,5);
+	q1_dot = q_dot(i,1); q2_dot = q_dot(i,2); q3_dot = q_dot(i,3); 
+	q4_dot = q_dot(i,4); q5_dot = q_dot(i,5);
+
+	qd1_dot = dq_des(i,1); qd2_dot = dq_des(i,2); qd3_dot = dq_des(i,3);
+	qd4_dot = dq_des(i,4); qd5_dot = dq_des(i,5);
+
+	qd1_ddot = ddq_des(i,1); qd2_ddot = ddq_des(i,2); qd3_ddot = ddq_des(i,3); 
+	qd4_ddot = ddq_des(i,4); qd5_ddot = ddq_des(i,5);
+	g = 9.81;
+
+	regressore;
+	piArray_dot = (R^(-1) * Y' * s')';  
+    piArray(i,:) = piArray(i-1,:) + delta_t*piArray_dot;
+	%% Progresso Simulazione
+    if mod(i,100) == 0
+        
+        fprintf('Percent complete: %0.1f%%.',100*i/(length(t)-1));
+        hms = fix(mod(toc,[0, 3600, 60])./[3600, 60, 1]);
+        fprintf(' Elapsed time: %0.0fh %0.0fm %0.0fs. \n', ...
+            hms(1),hms(2),hms(3));
     end
-%% Backstepping with correct estimation
-results_computed_torque1 = q0;
-index = 1;
-q = q0;
-dq = q_dot0;
-ddq = [0 0 0 0 0 ];
-for i=1:length(t)
-
-   % Error and derivate of the error  
-   
-    err = transpose(q_des(:,i)) - q;
-    derr = transpose(dq_des(:,i)) - dq;
     
-%     %Get dynamic matrices
-
-    M = KUKA.inertia(q); 
-    C = KUKA.coriolis(q,dq); 
-    G = KUKA.gravload(q); 
-    
-    tau = ( M*(ddq_des(:,i) + Kv*(derr') + Kp*(err')) + (dq*C)' + G' )';
-      
-    % Robot joint accelerations
-    ddq_old = ddq;
-    ddq = (pinv(M)*(tau' - (dq*C)'- G'))';
-        
-    % Tustin integration
-    dq_old = dq;
-    dq = dq + (ddq_old + ddq) * delta_t / 2;
-    q = q + (dq + dq_old) * delta_t /2;
-    
-    % Store result for the final plot
-    results_computed_torque1(index,:) = q;
-    index = index + 1;
-
-end
-%% Backstepping with wrong estimation
-results_computed_torque2 = q0;
-index = 1;
-q = q0;
-dq = q_dot0;
-ddq = [0 0 0 0 0 ];
-for i = 1:length(t)
-
-   % Error and derivate of the error   
-    err = transpose(q_des(:,i)) - q;
-    derr = transpose(dq_des(:,i)) - dq;
-    
-%     %Get dynamic matrices
-    M1 = KUKAmodel.inertia(q); 
-    C1 = KUKAmodel.coriolis(q,dq); 
-    G1 = KUKAmodel.gravload(q); 
-    
-    %% BackStepping Controller with wrong estimation'
-    
-    tau = ( M1*(ddq_des(:,i) + Kv*(derr') + Kp*(err')) + (dq*C1)' + G1' )';
-      
-    % Robot joint accelerations
-    ddq_old = ddq;
-    ddq = (pinv(M)*(tau' - (dq*C)'- G'))';
-        
-    % Tustin integration
-    dq_old = dq;
-    dq = dq + (ddq_old + ddq) * delta_t / 2;
-    q = q + (dq + dq_old) * delta_t /2;
-    
-    % Store result for the final plot
-    results_computed_torque2(index,:) = q;
-    index = index + 1;
-
 end
 %% Plot backstepping results for trajectory tracking
 %save('backstepping_adaptive','results_backstepping1','results_backstepping2','q_des')
 %load('backstepping_adaptive.mat')
-num_of_joints = 5;
-figure
-for j=1:num_of_joints
-    subplot(4,2,j);
-    plot(t(1:10001), results_backstepping1(1:10001,j))
-    hold on
-    plot (t,q_des(j,1:length(t)))
-    plot(t(1:10001), results_backstepping2(1:10001,j))
-    legend ('BackStepping','Desired angle', 'BackStepping with wrong estimation')
-    grid;
-end
+% num_of_joints = 5;
+% figure
+% for j=1:num_of_joints
+%     subplot(4,2,j);
+%     plot(t(1:10001), q(1:10001,j))
+%     hold on
+%     plot (t,q_des(j,1:length(t)))
+%     legend ('BackStepping_adattivo','Desired angle')
+%     grid;
+% end
 	case 4
 %%---------------------COMPUTED_TORQUE_ADAPTIVE---------------------------
 %%	Perturbazione iniziale dei parametri
