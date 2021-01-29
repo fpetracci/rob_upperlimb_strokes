5adc689dba41e91be2958e3c1e11e123a7daed48
fpetracci <petracci.francesco@gmail.com>
2021-01-29 16:04:00 +0100

    zero dynamic some improvements

--- Tavole/SistemiNL/FL_gru_4_con_smorza.m
+++ Tavole/SistemiNL/FL_gru_4_con_smorza.m
@@ -14,9 +14,6 @@ x = [x_t; x_t_dot; y_t; y_t_dot; theta; theta_dot; phi; phi_dot; L ; L_dot];
 syms  g b real
 z_t = 0;
 
-% Stato iniziale Numerico
-x0 = [0; 0; 0; 0; pi/4; pi/20; pi/4; pi/20; 2; 0] ;
-
 % posizione palla 
 x_ball = x_t + L*sin(theta)*sin(phi);
 y_ball = y_t + L*sin(theta)*cos(phi);
@@ -74,6 +71,7 @@ g3 = [0;...
 	  0;...
 	  0;...
 	  1];
+  
 G = [g1 g2 g3];
 
 % Uscite 
@@ -91,18 +89,30 @@ y = [x_ball - x_t; y_ball - y_t; z_ball-z_t ]; % pos relativa palla
 
 %% Cambio variabili
 syms xi_1 xi_2 xi_3 xi_4 xi_5 xi_6
-xi = [xi_1; xi_2; xi_3;xi_4;xi_5; xi_6];
+xi = [xi_1; xi_2; xi_3; xi_4; xi_5; xi_6];
 % lo stato osservabile del sistema linearizzato
 
 syms zeta_1 zeta_2 zeta_3 zeta_4 real
 zeta = [zeta_1; zeta_2; zeta_3; zeta_4]; 
 % lo stato non osservabile del sistema linearizzato
 
-% con controllo perfetto xi->0
+% La dinamica di ζ e` data da:
+%		ζ_dot = q(ζ, ξ) + p(ζ, ξ)ν
+% Ma in ζ_dot compaiono direttamente gli ingressi u dato che:
+%		ζ = [x_t; x_t_dot; y_t; y_t_dot]
+% e 
+%		u = [ x_t_ddot;  y_t_ddot]
+%
+% Dobbiamo ricavare u(ξ) cosi` da avere tutto in funzione di ξ, ζ
+
+% Partendo da
+%		u = −inv(E(x)) Γ(x) + inv(E(x)) ν
+% e ponendo ν = 0 ( studiamo solo zero dinamica)
+
 u_zerodin = -inv(E)*T;
 u_zerodin_12 = simplify(u_zerodin(1:2,1)); 
-% prendo solo: x_t_ddot e y_t_ddot
-% L_ddot non entra nel calcolo della dinamica di zeta
+% Ci occorre solo: x_t_ddot e y_t_ddot dato che L_ddot non entra nel 
+% calcolo della dinamica di zeta
 
 PHI = [Lf_full_mimo;x_t;x_t_dot;y_t;y_t_dot];
 % PHI = [zeta; xi]
@@ -146,60 +156,53 @@ M = [M_11	M_12	M_13;...
 
 B = [xi_2; xi_4; xi_6]; % termine "noto"
 
+solution = linsolve(M,B);
 
+L_dot_zerodin = simplify(solution(1));
+phi_dot_zerodin = simplify(solution(2));
+theta_dot_zerodin = simplify(solution(3));
 
-%% ARRIVATI QUI
-X = linsolve(M,B);
-L_dot = simplify(X(1))
-phi_dot = simplify(X(2))
-theta_dot = simplify(X(3))
-U_1_2 = [-(sin(phi)*(g*sin(theta) + L*b*theta_dot))/cos(theta)...;
-		 -(cos(phi)*(g*sin(theta) + L*b*theta_dot))/cos(theta)];
-U_1_2 = simplify(U_1_2);
-%subs(U_1_2,XI,[0; 0; 0; 0; 0; 0]) error division by zero
- 
-
-%% Zero dinamica
-
-syms x_t_ddot y_t_ddot L_ddot
-u = [x_t_ddot; y_t_ddot; L_ddot]
-upsi = T + E * u
-upsi = simplify(upsi,5)
-
-% creazione uspilon ingressi "linearizzati"
-ddy1 = L_dot*(phi_dot*cos(phi)*sin(theta) + theta_dot*cos(theta)*sin(phi)) + phi_dot*(L_dot*cos(phi)*sin(theta) + L*theta_dot*cos(phi)*cos(theta) - L*phi_dot*sin(phi)*sin(theta)) + theta_dot*(L_dot*cos(theta)*sin(phi) + L*phi_dot*cos(phi)*cos(theta) - L*theta_dot*sin(phi)*sin(theta)) - L*cos(theta)*sin(phi)*(b*theta_dot - (phi_dot^2*sin(2*theta))/2 + (g*sin(theta))/L + (2*L_dot*theta_dot)/L) - L*cos(phi)*sin(theta)*(2*phi_dot*theta_dot*cot(theta) + (2*L_dot*phi_dot)/L) + ...
-		(- cos(phi)^2 - cos(theta)^2*sin(phi)^2) * u(1,:) +...
-		(cos(phi)*sin(phi) - cos(phi)*cos(theta)^2*sin(phi)) * u(2,:) + ...
-		sin(phi)*sin(theta) * u(3,:);
-
-ddy2 = L_dot*(theta_dot*cos(phi)*cos(theta) - phi_dot*sin(phi)*sin(theta)) - theta_dot*(L*phi_dot*cos(theta)*sin(phi) - L_dot*cos(phi)*cos(theta) + L*theta_dot*cos(phi)*sin(theta)) - phi_dot*(L_dot*sin(phi)*sin(theta) + L*phi_dot*cos(phi)*sin(theta) + L*theta_dot*cos(theta)*sin(phi)) - L*cos(phi)*cos(theta)*(b*theta_dot - (phi_dot^2*sin(2*theta))/2 + (g*sin(theta))/L + (2*L_dot*theta_dot)/L) + L*sin(phi)*sin(theta)*(2*phi_dot*theta_dot*cot(theta) + (2*L_dot*phi_dot)/L)+...
-	(cos(phi)*sin(phi) - cos(phi)*cos(theta)^2*sin(phi))* u(1,:) +...
-	(- sin(phi)^2 - cos(phi)^2*cos(theta)^2)* u(2,:) + ...
-	cos(phi)*sin(theta) * u(3,:);
-
-ddy3 = L*sin(theta)*(b*theta_dot - (phi_dot^2*sin(2*theta))/2 + (g*sin(theta))/L + (2*L_dot*theta_dot)/L) - L_dot*theta_dot*sin(theta) - theta_dot*(L_dot*sin(theta) + L*theta_dot*cos(theta))+...
-	cos(theta)*sin(phi)*sin(theta) * u(1,:) + ...
-	cos(phi)*cos(theta)*sin(theta) * u(2,:) + ...
-	cos(theta) *u(3,:);
-
-% suffisso _lin per cercare retroazione linearizzante
-upsi_lin = [ddy1; ddy2; ddy3]
-upsi_lin = simplify(upsi_lin,5)
-u_lin = -inv(E) * T + inv(E) * upsi_lin;
-u_lin = simplify(u_lin,5)
-
-zeta =[x_t; x_t_dot; y_t; y_t_dot]
-zeta_dot = [jacobian(x_t,x)*(f + G*u);...
-	jacobian(x_t_dot,x)*(f + G*u);...
-	jacobian(y_t,x)*(f + G*u);...
-	jacobian(y_t_dot,x)*(f + G*u)]
-
-% % zeta_dot_mat = 
-PHI_full = jacobian([Lf_full_mimo;zeta],x)
-rank(PHI_full)
+% sanity check
+% M*solution = B 
 
 %
-upsi_0	= subs(subs(upsi, x, x0), [b g], [1 9.81])
-upsi_00 = eval(subs(upsi_0, u, [0;0;0]))
-u_lin0	= eval(subs(subs(-inv(E) * T + inv(E) * upsi_00, x, x0), [b g], [1 9.81]) ) 
+%-(sin(phi)*(g*sin(theta) + L*b*theta_dot))/cos(theta)
+% -(cos(phi)*(g*sin(theta) + L*b*theta_dot))/cos(theta)
+u_zerodin_xi = [	-(sin(phi_zerodin)*(g*sin(theta_zerodin) + L_zerodin*b*theta_dot_zerodin))/cos(theta_zerodin);...
+					-(cos(phi_zerodin)*(g*sin(theta_zerodin) + L_zerodin*b*theta_dot_zerodin))/cos(theta_zerodin)];
 
+% error				
+% u_zerodin_xi0 = subs(u_zerodin_xi, xi, [0; 0; 0; 0; 0; 0])
+
+
+%% Prova Con limiti
+
+% j e` una singola variabile che tende a zero. Facciamo convergere a zero
+% ogni elemento di xi nello stesso modo di j
+syms j real
+xi_4lim = subs(xi, xi, [ j; j; j; j; j; j]);
+
+u_zerodin_xi_4lim = subs(u_zerodin_xi, xi, xi_4lim);
+% j si semplifica ovunque
+u_zerodin_xi0 = limit(u_zerodin_xi_4lim, j, 0) % u(xi=0)
+
+%% Studio della Dinamica di zeta
+% in generale
+% ζ_dot =  q(ξ; ζ) + p(ξ; ζ) * upsi
+zeta_dot = [ 0 1 0 0;...
+			0 0 0 0;...
+			0 0 0 1;...
+			0 0 0 0] * zeta + [0; u_zerodin_xi(1); 0; u_zerodin_xi(2)]
+
+% zeta_dot(xi = 0, zeta)
+% ζ_dot = q(0; ζ) sia asintoticamente stabile
+zeta_dot0 = subs(zeta_dot, u_zerodin_xi, u_zerodin_xi0);
+
+% A = [ 0 1 0 0;...
+% 	0 0 0 0;...
+% 	0 0 0 1;...
+% 	0 0 0 0];
+% forma di jordan, modi propri 1,t quindi instabile polinomialmente.
+
+
+	
